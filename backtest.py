ccb39b82cf29031f3bfe50aa7705ed99c59388e3
ahmedfadhil <ahmed.fadhil@alumni.emu.edu.tr>
2018-11-06 16:20:31 +0100

    finished the backtest and called data from quandl

--- backtest.py
+++ backtest.py
@@ -20,7 +20,6 @@ class Strategy(object):
         containing the signals to go long, short or hold (1, -1 or 0)."""
         raise NotImplementedError("Should implement generate_signals()!")
 
-
 class Portfolio(object):
     """An abstract base class representing a portfolio of
     positions (including both instruments and cash), determined
@@ -31,18 +30,17 @@ class Portfolio(object):
     @abstractmethod
     def generate_positions(self):
         """Provides the logic to determine how the portfolio
-                positions are allocated on the basis of forecasting
-                signals and available cash."""
+        positions are allocated on the basis of forecasting
+        signals and available cash."""
         raise NotImplementedError("Should implement generate_positions()!")
+
     @abstractmethod
     def backtest_portfolio(self):
-         """Provides the logic to generate the trading orders
+        """Provides the logic to generate the trading orders
         and subsequent equity curve (i.e. growth of total equity),
         as a sum of holdings and cash, and the bar-period returns
         associated with this curve based on the 'positions' DataFrame.
 
         Produces a portfolio object that can be examined by
         other classes/functions."""
-         raise NotImplementedError("Should implement backtest_portfolio()!")
-
-    
+        raise NotImplementedError("Should implement backtest_portfolio()!")
